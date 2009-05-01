2eb8e36b4ff3e8c39eb146144fbcc33ef3ede059
JCampbell1 <JCampbell1@84d64312-f151-11dd-9fdb-3debd5008f28>
2009-05-01 17:01:21 +0000

    Massive speed up of time to sort the rows

--- trunk/incl/js/jquery.tablesorter.js
+++ trunk/incl/js/jquery.tablesorter.js
@@ -1,42 +1,54 @@
 /**
  * Known issues:
  * 1) Does not work with colspans
- * 2) Issue when th's within table body.  Unpredictable behavior.
+ * 2) Issue when th's within table body.	Unpredictable behavior.
  */
- (function($) {
+(function($) {
 
-   var comparators = {
-     STRING: function(a,b) {
-       // Separates and sorts by case
-       return $(a).text() == $(b).text() ? 0 :
-                 $(a).text() > $(b).text() ? 1 : -1;
-     },
-     NUMERIC: function(a,b) {
-       return parseFloat($(a).text()) == parseFloat($(b).text()) ? 0 :
-                 parseFloat($(a).text()) > parseFloat($(b).text()) ? 1 : -1;
-     },
-     STRING_INSENSITIVE: function(a, b) {
-       return $(a).text().toLowerCase() == $(b).text().toLowerCase() ? 0 :
-                 $(a).text().toLowerCase() > $(b).text().toLowerCase() ? 1 : -1;
-     }
-   }
+	var comparators = {
+		STRING: function(a,b) {
+			// Separates and sorts by case
+			return a > b ? 1 : -1;
+		},
+		NUMERIC: function(a,b) {
+			return parseFloat(a) > parseFloat(b) ? 1 : -1;
+		},
+		STRING_INSENSITIVE: function(a, b) {
+			return a.toLowerCase() > b.toLowerCase() ? 1 : -1 ;
+		}
+	}
 
-   $.fn.tsort = function(column,direction,compare) {
-     var d = direction == -1 ? -1 : !!direction ? 1 : -1;
+	$.fn.tsort = function(column,direction,compare) {
+		var d = direction == -1 ? -1 : !!direction ? 1 : -1;
 
-     var comp = $.isFunction(compare) ? compare :
-                   comparators[compare] ? comparators[compare] : comparators.STRING_INSENSITIVE;
+		var comp = $.isFunction(compare) ? compare :
+									 comparators[compare] ? comparators[compare] : comparators.STRING_INSENSITIVE;
+									
 
-     this.each(function() {
-       var $table = $(this);
-       var arrayRows = $(this).find('tbody tr').get();
-       arrayRows.sort(function(rowA,rowB) {
-         return d*comp($(rowA).children('td:eq('+column+')').get(0),$(rowB).children('td:eq('+column+')').get(0));
-       });
-       $(arrayRows).each(function() {
-         $table.find('tbody:first').append(this);
-       });
-     });
-   }
- })(jQuery);
+		return this.each(function() {
+			var $table = $(this);
+			var arrayRows = $(this).find('tbody tr').get();
+			
+			/* this is a preformance hack.  We precalculate the text we are going to compare, and 
+			 	 pass that to the sort function.  This is much faster, than including this code in the sort
+			*/	
+			for(var i in arrayRows) {
+				arrayRows[i] = {"dom_element":arrayRows[i],
+												"compare_text":$('td',arrayRows[i])[column].innerHTML
+												};
+			}
+			
+			arrayRows.sort(function(rowA,rowB) {
+				return d*comp(rowA.compare_text,rowB.compare_text );
+			});
+			
+			// rebuild the array of dom elements
+			for(var i in arrayRows) {
+				arrayRows[i] = arrayRows[i].dom_element
+			}
+			$table.find('tbody').append( $(arrayRows) );  // append is faster than prepend
+		
+		});
+	}
+})(jQuery);
 
